574442ed97bc0f715114ba9968f23415d92ee3ac
sunilrav <rsunilbabu@gmail.com>
2013-06-07 17:02:06 -0600

    Initial

---
+++ Wishlist/Wishlist.Service.Tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Wishlist.Service.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
---
+++ Wishlist/Wishlist.Web/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Wishlist.Web.MvcApplication" Language="C#" %>
